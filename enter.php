432b6ca5ab3a342827cdebdfbe59d170df4a59d8
Alexandr Martyniv <aamartyniv@gmail.com>
2014-11-12 20:18:21 +0200

    initial commit

---
+++ enter.php
@@ -0,0 +1,142 @@
+<!DOCTYPEhtml>
+
+<html>
+
+<head>
+    <title> First page </title>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+</head>
+
+<body>
+
+<div class="intend">
+    <div class="row">
+        <div class="col-md-3"></div>
+
+        <div class="col-md-7">
+
+            <div class="row">
+
+                <div class="col-md-10">
+
+                    <div class="index-data">
+
+                        <h1 class="index-page"><small><span class="glyphicon glyphicon-heart" ></span></small>
+                            <em> One + enO </em>
+                            <small><span class="glyphicon glyphicon-heart" ></span></small>
+                        </h1>
+
+                        <p class="lead"><em>You searching for love. You want love someone. You looking for your destiny.Welcome to our site.
+                                <br>Here you can find many profiles of other clients. Read them. Speak with them. And find your ideal partner.
+                                <br>We want to make you fallhead over heels.</em>
+                        </p>
+
+                     </div>
+
+                </div>
+
+                <div class="col-md-3"></div>
+                <div class="col-md-4">
+
+                    <div class="panel panel-default">
+                        <div class="panel-body">
+
+                            <form class="form-horizontal" role="form" name="enter" method="post" action="">
+                                <div class="form-group">
+
+                                    <label class="col-sm-12 control-label sr-only" for="email"> Email </label>
+
+                                    <div class="col-xs-12">
+                                        <input type="email" class="form-control" id="email"  placeholder="Enter email">
+                                    </div>
+
+                                </div>
+
+                                <div class="form-group">
+
+                                    <label for="password" class="col-sm-12 control-label sr-only"> Password </label>
+
+                                    <div class="col-sm-12">
+                                        <input  type="password" class="form-control" id="password" maxlength="20" placeholder="Password">
+                                    </div>
+
+                                </div>
+
+
+                                <div class="form-group">
+
+                                    <div class="col-sm-offset-1 col-sx-5">
+
+                                        <div class="checkbox">
+                                            <label>
+                                                <input type="checkbox"> Remember me
+                                            </label>
+                                        </div>
+
+                                    </div>
+
+                                </div>
+
+                                <div class="form-group">
+
+                                    <div class="col-sm-offset-1 col-sx-4">
+                                        <button type="submit" class="btn btn-primary btn-sm"> Welcome  </button>
+                                    </div>
+
+                                </div>
+                                </form>
+
+                             </div>
+                        </div>
+
+
+
+                        <div class="panel panel-default">
+                            <div class="panel-body">
+
+                                <form class="form-horizontal" role="form" name="registration" action="" method="post">
+
+                                    <div class="form-group">
+                                        <label class="col-sm-12 control-label sr-only" for="email"> Email </label>
+                                        <div class="col-xs-12">
+                                            <input type="email" class="form-control" id="email"  placeholder="Enter email">
+                                        </div>
+                                    </div>
+
+
+                                    <div class="form-group">
+                                        <label for="password" class="col-sm-12 control-label sr-only"> Password </label>
+
+                                        <div class="col-xs-12">
+                                            <input  type="password" class="form-control" id="password" maxlength="20" placeholder="Password">
+                                        </div>
+                                    </div>
+
+
+                                    <div class="form-group">
+                                        <label for="repeat password" class="col-sm-12 control-label sr-only"> Repeat password </label>
+
+                                        <div class="col-sm-12">
+                                            <input  type="password" class="form-control" id="repeat password" maxlength="20" placeholder="Repeat password">
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group">
+
+                                        <div class="col-sm-offset-0 col-sm-1">
+                                            <button type="submit" class="btn btn-primary btn-sm"> Registration  </button>
+
+                                        </div>
+                                    </div>
+                                </form>
+                                </div>
+                            </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
